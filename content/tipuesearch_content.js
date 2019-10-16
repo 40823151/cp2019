56d35d43702ada9ab60ef22258e9ba79e60d0f29
40823151 <40823151@gm.nfu.edu.tw>
2019-10-16 23:23:10 +0800

    initial add

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '', 'tags': '', 'url': 'About.html'}, {'title': 'supplement', 'text': '', 'tags': '', 'url': 'supplement.html'}, {'title': 'USB', 'text': '', 'tags': '', 'url': 'USB.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '', 'tags': '', 'url': 'About.html'}, {'title': 'supplement', 'text': '\n', 'tags': '', 'url': 'supplement.html'}, {'title': 'happy cat', 'text': '', 'tags': '', 'url': 'happy cat.html'}, {'title': 'USB', 'text': '', 'tags': '', 'url': 'USB.html'}]};
