9888ca49263640a24fedd63672fc6df6fc0966af
40823151 <40823151@gm.nfu.edu.tw>
2020-01-08 20:04:14 +0800

    reavel 溫度轉換

--- downloads/dart_ex/weight_change/weight_change.dart
+++ downloads/dart_ex/weight_change/weight_change.dart
@@ -1,3 +1,4 @@
+/* 
 import "dart:html";
 
 lbtokg(num lb){
@@ -32,5 +33,37 @@ convert(String data){
      output.innerHtml = " $number KG =  ${kgtolb(number).toStringAsFixed(4)} LB";
   } else {
      output.innerHtml = "請輸入數字加上 LB 或 KG!";
+  }
+} 
+*/
+// 因為要使用超文件表單, 因此導入 html 程式庫
+import "dart:html";
+
+kgtolb(num kg){
+return kg*2.2046223;
+}
+
+lbtokg(num lb){
+return lb*0.45359243;
+}
+main() {
+    InputElement tempInput = querySelector("#weight");
+    querySelector("#submit").onClick.listen((e) => convert(tempInput.value));
+}
+convert(String data){
+  int len;
+  var type;
+  var number;
+  LabelElement output = querySelector("#output");
+  len = data.length;
+  type = data[len-2];
+  number = data.substring(0, len-2);
+  number = int.parse(number);
+  if (type == "k"){
+      output.innerHtml =" $number 公斤 =  ${kgtolb(number).toStringAsFixed(2)} 磅";
+    }else if (type == "l"){
+     output.innerHtml =" $number 磅 =  ${kgtolb(number).toStringAsFixed(2)} 公斤";
+    } else {
+     output.innerHtml = "請輸入數字加上 kg 或 lb!";
   }
 } 
